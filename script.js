90f34b4cb4b0850bfb344b498ba4a8c49273ef8b
Harsh <harshsharma41664@gmail.com>
2023-02-08 01:43:08 +0530

    60% done with the project

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 class BookBg{
-    constructor(){
+    constructor(name,author,pages,status){
         let b = document.createElement('div');
-        b.style="width:289px; height: 347px;";
-        let bimg = document.createElement('img');
+        b.className="dabook";
         b.style.backgroundImage="url(./images/book.svg)";
-        b.style.border="1px solid black";
+        b.innerHTML=`<div class="ykb"><div class="y">Name: ${name}</div>
+        <div class="y">Author: ${author}</div><div class="y">Pages: ${pages}</div>
+        <div class="y">Status: ${status}</div>
+        </div>`;
         this.bookDiv = b;
     }
 }
 let main = document.getElementsByClassName('books');
-let dune = Object.create(new BookBg());
-let sapiens = Object.create(new BookBg());
+let dune = Object.create(new BookBg("Dune","Frank",778,"Read"));
+let sapiens = Object.create(new BookBg("Sapiens","Frank",778,"Read"));
+
 const Books = [dune,sapiens];
 
+Books.forEach(book=>{
+    console.log("I love");
+    main[0].appendChild(book.bookDiv);
+})
 
 
 
