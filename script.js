71bf529e9751399bf76fc2953ea4a9d3a8f00eb6
Harsh <harshsharma41664@gmail.com>
2023-02-09 00:26:32 +0530

    Project done but code is bad

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-class BookBg{
+class Book{
     constructor(name,author,pages,status){
         let b = document.createElement('div');
         b.className="dabook";
         b.style.backgroundImage="url(./images/book.svg)";
+        //for now I'm using Inner HTML and not changing it because I'm lazy
+        //In the future, NEVER use InnerHTML ever.
         b.innerHTML=`<div class="ykb"><div class="y">Name: ${name}</div>
         <div class="y">Author: ${author}</div><div class="y">Pages: ${pages}</div>
         <div class="y">Status: ${status}</div>
-        </div>`;
+        </div>
+        <input type="button" class="yr" value="remove">`;
         this.bookDiv = b;
     }
 }
 let main = document.getElementsByClassName('books');
-let dune = Object.create(new BookBg("Dune","Frank",778,"Read"));
-let sapiens = Object.create(new BookBg("Sapiens","Frank",778,"Read"));
+let dune = Object.create(new Book("Dune","Frank",778,"Read"));
+let sapiens = Object.create(new Book("Sapiens","Frank",778,"Read"));
 
 const Books = [dune,sapiens];
 
 Books.forEach(book=>{
-    console.log("I love");
     main[0].appendChild(book.bookDiv);
 })
+
+let nameform = document.getElementById('bname');
+let authform = document.getElementById('aname');
+let pageform = document.getElementById('pages');
+let statform = document.getElementById('status');
+let submitform = document.getElementById('addbtn')
+let remove = document.querySelectorAll('.yr');
+let rc = (e)=>{
+    e.target.parentNode.remove();
+    remove = document.querySelectorAll('.yr');
+}
+let formInputs = [nameform,authform,pageform,statform];
+submitform.addEventListener('click',()=>{
+    let arr = []
+    formInputs.forEach(x=>{
+        arr.push(x.value);
+    });
+    let tbook = Object.create(new Book(arr[0],arr[1],arr[2],arr[3]));
+    if(arr[0]!=""&&arr[1]!=""&&arr[2]!=""&&arr[3]!=""){
+        Books.push(tbook);
+        main[0].appendChild(tbook.bookDiv);
+        remove = document.querySelectorAll('.yr');
+        remove[remove.length-1].addEventListener('click',rc);
+    }
+});
+
+
+remove.forEach((btn)=>{
+    btn.addEventListener('click',rc);
+});
+
+
 
 
 
